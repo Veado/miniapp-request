e0d18f61284af79b028c3b001a1fc27f2420121e
phill <freecorleone988@gmail.com>
2018-09-03 16:52:40 +0800

    promise

--- defaultConf.js
+++ defaultConf.js
@@ -7,7 +7,9 @@ var defaultConfig  = {
   header: defaultContentType,
   baseUrl: '',
   timeout: null,
-  before: function(config) {return config}
+  before: function(config) {return new Promise(function(resolve){
+    resolve(config)
+  })}
 }
 
 module.exports = defaultConfig
--- request.js
+++ request.js
@@ -2,31 +2,27 @@ var defaultConfig = require('./defaultConf')
 var merge = require('./util/merge')
 
 function Request(config) {
-  // console.log('this', this)
-  // var defaults = merge(defaultConfig, config)
-  // console.log('config', defaults)
   this.defaults = merge(defaultConfig, config)
 }
 
 Request.prototype.request = function(config) {
   
-  var config = merge({}, this.defaults, config)
-
-  config = config.before(config)
-  
   return new Promise(function(resolve, reject){
-    // console.log('this',this)
-    wx.request({
-      url: config.baseUrl + config.url, //仅为示例，并非真实的接口地址
-      data: config.data,
-      header: config.header,
-      success: function(res) {
-        resolve(res)
-      },
-      fail: function(e) {
-        reject(e)
-      }
-    })
+    var config = merge({}, this.defaults, config)
+
+    config.before(config).then(function(config){
+      wx.request({
+        url: config.baseUrl + config.url, //仅为示例，并非真实的接口地址
+        data: config.data,
+        header: config.header,
+        success: function(res) {
+          resolve(res)
+        },
+        fail: function(e) {
+          reject(e)
+        }
+      })
+    }.bind(this))
   }.bind(this))
 }
 
